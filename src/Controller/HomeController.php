c936157e8b357d85e6f7db3a652ec6987458bbd8
d0m G <dom@ginhac.com>
2023-03-31 15:41:46 +0200

    FEAT: under construction page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'under_construction')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/under-construction.html.twig', [
             'title' => 'Welcome to the atlas challenge'
         ]);
     }
